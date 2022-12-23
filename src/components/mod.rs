8fcc362a0195d2c2dc37ead106522437f938ff45
Max Breitenfeldt <git@mxbr.me>
2022-12-23 14:20:53 +0100

    remove dummy tracker

--- src/components/mod.rs
+++ src/components/mod.rs
@@ -2,10 +2,8 @@ pub mod encoder;
 pub mod matcher;
 pub mod python_runner;
 pub mod sampler;
-pub mod tracker;
 
 pub use encoder::{VideoEncoder, VideoEncoderSettings};
 pub use matcher::Matcher;
 pub use python_runner::PythonRunner;
 pub use sampler::Sampler;
-pub use tracker::Tracker;
