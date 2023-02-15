910f73b9406c8b848902a2ee4e5327d10cfcc50a
Max Breitenfeldt <git@mxbr.me>
2023-02-15 13:48:00 +0100

    build: enable pylon for appimage

--- package/build_appimage.sh
+++ package/build_appimage.sh
@@ -2,14 +2,18 @@
 
 source scl_source enable llvm-toolset-7.0
 export PROTOC=/opt/protoc/bin/protoc
+export PYLON_ROOT=/opt/pylon5
+BUILD_DIR=/tmp/biotracker_build
+APPDIR=$BUILD_DIR/AppDir
 
 set -e
 
-rm -rf AppDir
-cargo build --release
+mkdir $BUILD_DIR || true
+cargo build --release --target-dir $BUILD_DIR --features pylon
+rm -rf $APPDIR || true
 /opt/linuxdeploy/usr/bin/linuxdeploy \
-  -e target/release/biotracker4 \
-  --appdir AppDir \
+  -e $BUILD_DIR/release/biotracker4 \
+  --appdir $APPDIR \
   --desktop-file package/resources/biotracker4.desktop \
   --icon-file package/resources/biotracker4.png \
   --output appimage \
