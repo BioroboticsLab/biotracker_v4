78dc72e8798fea2c1f0ab8472f0a8c97ac4501fb
Max Breitenfeldt <git@mxbr.me>
2024-02-24 18:18:29 +0100

    improve python logging

--- src/biotracker/python_process.rs
+++ src/biotracker/python_process.rs
@@ -6,7 +6,8 @@ use tokio::io::{AsyncBufReadExt, BufReader};
 use tokio::process::Command;
 
 pub struct PythonProcess {
-    log_task: tokio::task::JoinHandle<Result<()>>,
+    stdout_log_task: tokio::task::JoinHandle<Result<()>>,
+    stderr_log_task: tokio::task::JoinHandle<Result<()>>,
     wait_task: tokio::task::JoinHandle<Result<()>>,
     id: String,
 }
@@ -18,33 +19,44 @@ impl PythonProcess {
         address: String,
     ) -> Result<Self> {
         let commandline = format!(
-            "export BIOTRACKER_COMPONENT_ADDRESS='{}'; . '{}/bin/activate'; exec python3 {} 2>&1",
+            "export BIOTRACKER_COMPONENT_ADDRESS='{}'; . '{}/bin/activate'; exec python3 {}",
             address, python_config.venv, python_config.cmd
         );
         let mut child = Command::new("/bin/sh")
             .arg("-c")
             .arg(commandline)
             .stdout(Stdio::piped())
+            .stderr(Stdio::piped())
             .kill_on_drop(true)
             .spawn()?;
         let mut stdout_reader =
+            BufReader::new(child.stderr.take().context("stdout not available")?).lines();
+        let mut stderr_reader =
             BufReader::new(child.stdout.take().context("stdout not available")?).lines();
-        let id = config.id.clone();
-        let log_task = tokio::spawn(async move {
+        let stdout_id = config.id.clone();
+        let stdout_log_task = tokio::spawn(async move {
             while let Some(line) = stdout_reader.next_line().await? {
-                log::warn!(target: &id, "{}", line);
+                log::warn!("{}: {}", stdout_id, line);
+            }
+            Ok(())
+        });
+        let stderr_id = config.id.clone();
+        let stderr_log_task = tokio::spawn(async move {
+            while let Some(line) = stderr_reader.next_line().await? {
+                log::error!("{}: {}", stderr_id, line);
             }
             Ok(())
         });
         let id = config.id.clone();
         let wait_task = tokio::spawn(async move {
             let status = child.wait().await?;
-            log::error!(target: &id, "Python process exited with status {}", status);
+            log::error!("{}: Python process exited with status {}.", id, status);
             Ok(())
         });
 
         Ok(Self {
-            log_task,
+            stdout_log_task,
+            stderr_log_task,
             wait_task,
             id: config.id.clone(),
         })
@@ -57,7 +69,8 @@ impl PythonProcess {
                 log::warn!("Failed to kill process {}: {}", self.id, e);
             }
         }
-        self.log_task.abort();
+        self.stdout_log_task.abort();
+        self.stderr_log_task.abort();
     }
 
     async fn kill(&mut self) -> Result<()> {
